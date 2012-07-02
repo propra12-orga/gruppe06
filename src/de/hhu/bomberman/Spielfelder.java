4af1952408f49172d732da3552e8276837ff94e8
Tuba Ciftci <tuba.ciftci@uni-duesseldorf.de>
2012-07-02 13:16:58 +0200

    Spielfelder

---
+++ src/de/hhu/bomberman/Spielfelder.java
@@ -0,0 +1,36 @@
+public class Spielfeld2 {
+
+	public double size = 0;
+	public boolean belegt = false;
+	public double x;
+	public double y;
+	public boolean mauer = false;
+	public int bomben_number;
+	public String beinhaltet;
+
+	/**
+	 * Initialisierung des Spielfeldes
+	 * @param d x
+	 * @param e y
+	 * @param spielfeldgroesse
+	 * @param s Inhalt
+	 * @param b Belegung Ja/Nein
+	 */
+	public Spielfeld2(double d, double e, double spielfeldgroesse, String s,
+			boolean b) {
+		x = d;
+		y = e;
+		size = spielfeldgroesse;
+		belegt = b;
+		beinhaltet = s;
+	}
+
+	public void show() {
+		StdDraw.show();
+	}
+
+	public void clear() {
+		StdDraw.clear();
+	}
+
+}
